f67548c8d93333e88e9ffdebd0c4ac94f1261385
Nikhil Tiwary <ntiwary61@gmail.com>
2023-12-06 17:39:15 +0530

    feat: remove jitters during drawing by using curve smoothing using Bezier Curves

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,21 +1,21 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Canvas({ currentColor }) {
   const canvasRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [prevPosition, setPrevPosition] = useState({ x: 0, y: 0 });
 
-  const startDrawing = (e) => {
-    setIsDrawing(true);
-
+  useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    //Get mouse coordinates
-    const x = e.nativeEvent.offsetX;
-    const y = e.nativeEvent.offsetY;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+  }, []);
 
-    ctx.fillStyle = currentColor;
-    ctx.fillRect(x, y, 5, 5);
+  const startDrawing = (e) => {
+    setIsDrawing(true);
+    setPrevPosition({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
   };
 
   const draw = (e) => {
@@ -24,11 +24,19 @@ export default function Canvas({ currentColor }) {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    const x = e.nativeEvent.offsetX;
-    const y = e.nativeEvent.offsetY;
+    //Get mouse coordinates
+    const currentX = e.nativeEvent.offsetX;
+    const currentY = e.nativeEvent.offsetY;
 
-    ctx.fillStyle = currentColor;
-    ctx.fillRect(x, y, 5, 5);
+    ctx.beginPath();
+    ctx.moveTo(prevPosition.x, prevPosition.y);
+    ctx.lineTo(currentX, currentY);
+
+    ctx.strokeStyle = currentColor;
+    ctx.lineWidth = 5; //to be changed into dynamic value
+    ctx.stroke();
+
+    setPrevPosition({ x: currentX, y: currentY });
   };
 
   const endDrawing = () => {
